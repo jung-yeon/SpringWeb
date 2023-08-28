c494dd8960aea8013aeb9db5c13bb013396a0d0b
이정연 <99dlwjddus@naver.com>
2023-08-28 17:27:14 +0900

    Spring web jUnit mySQL 삽입, 삭제 테스트 코드 추가

---
+++ src/test/java/org/zerock/ex1/Ex1ApplicationTest.java
@@ -0,0 +1,12 @@
+package org.zerock.ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Ex1ApplicationTest {
+    @Test
+    void contextLoads(){
+        System.out.println("contextLoads...");
+    }
+}
